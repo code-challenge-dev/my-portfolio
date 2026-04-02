c94c542e703313d758e07fbccbcb4a3860edf1cb
alexandr <alexandralexin12@gmail.com>
2026-04-01 20:09:54 -0400

    fix netlify config

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from 'vite';
 // https://vite.dev/config/
 export default defineConfig({
 	plugins: [react()],
-	base: '/my-portfolio/',
+	base: '/',
 	server: {
 		host: true,
 		open: true,
